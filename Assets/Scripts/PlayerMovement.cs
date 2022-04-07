391ac8c6fd33730a379c1e431ddf5ca068e0b2cb
Mohammed Mubarak <modymu9@gmail.com>
2022-04-07 19:22:58 +0200

    Added vertical movement
    I just added vertical movement and removed the z value because we don't touch z in 2D. cause it will then change the layer order in the game.
    eg . try to make  objects one over the other then change the z value and look what will happen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /* Simple movement script 
+        << short description >>
+    */
     public float speed = 50f;
-    float h;
-    // Update is called once per frame
+    float h, v;
+
     void Update()
     {
         h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
     }
+
     private void FixedUpdate()
     {
-        transform.position += new Vector3(h, 0f, 0f);
+        transform.position += new Vector3(h, v); // we don't change z in 2D games. it's used to sort objects
     }
 }
